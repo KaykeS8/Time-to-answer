0c1bd5b82b80305259bbb384a9e76ee018f84d0b
kayke simao <89744379+KaykeS8@users.noreply.github.com>
2023-12-29 17:10:10 -0300

    Implementação do metodo search e last_questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,12 @@ class Question < ApplicationRecord
   belongs_to :subject, inverse_of: :questions
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
+
+  def self.search(term, page)
+    self.includes(:answers).where("lower(description) LIKE ?", "%#{term.downcase}%").page(page)
+  end
+
+  def self.last_questions(page)
+    self.includes(:answers).order('created_at desc').page(page)
+  end
 end
