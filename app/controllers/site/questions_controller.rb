9c0a9ae101b4f9e7c7d9c780e1fdc335b6318d8d
kayke simao <89744379+KaykeS8@users.noreply.github.com>
2023-12-29 17:08:08 -0300

    Refatoração com o metodo last_questions para pegar as ultimas perguntas

--- app/controllers/site/questions_controller.rb
+++ app/controllers/site/questions_controller.rb
@@ -1,6 +1,6 @@
 class Site::QuestionsController < SiteController
   before_action :authenticate_user!, only: %i[ index ]
   def index
-    @questions = Question.includes(:answers).page(params[:page])
+    @questions = Question.last_questions(params[:page])
   end
 end
