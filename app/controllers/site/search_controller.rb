216f117cfa1a493de6597c41adfc3d6f2ceb5798
kayke simao <89744379+KaykeS8@users.noreply.github.com>
2023-12-29 17:09:02 -0300

    Utilização do search do question para fazer pesquisa de uma pergunta

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,9 +1,7 @@
 class Site::SearchController < SiteController
   
   def questions
-    @questions = Question.includes(:answers)
-                          .where("lower(description) LIKE ?", "%#{params[:term].downcase}%")
-                          .page(params[:page])
+    @questions = Question.search(params[:term], params[:page])
   end
 end
 
