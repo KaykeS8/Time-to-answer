39624ae101505c3088e46e5d255ca5e8a939dd1d
kayke simao <89744379+KaykeS8@users.noreply.github.com>
2024-01-03 19:09:03 -0300

    metodo para mostrar mensagem na partial question

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,12 @@
 module SiteHelper
+  def msg_header
+    case params[:action]
+      when 'index'
+        "Ultimas perguntas casdastradas..."
+      when 'questions'
+        "Resultados para o termo \"#{params[:term]}\""
+      when 'subject'
+        "Mostrando questões para o assunto #{params[:subject]}"
+    end
+  end
 end
