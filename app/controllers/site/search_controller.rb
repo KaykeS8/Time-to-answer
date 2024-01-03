545313918b4cc9aa23a549f8b59574f23cbf55fd
kayke simao <89744379+KaykeS8@users.noreply.github.com>
2024-01-03 19:08:22 -0300

    action subject adicionado para pesquisa de um subject

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,7 +1,10 @@
 class Site::SearchController < SiteController
-  
   def questions
     @questions = Question.search_question(params[:term], params[:page])
+  end
+
+  def subject 
+    @questions = Question.search_subject(params[:page], params[:subject_id])
   end
 end
 
