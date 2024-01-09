5bc3e7be51d93b49d59b52833512a69f58e90441
kayke simao <89744379+KaykeS8@users.noreply.github.com>
2024-01-09 18:07:16 -0300

    Controller para o perfil do usuario

--- app/controllers/users_backoffice/profile_controller.rb
+++ app/controllers/users_backoffice/profile_controller.rb
@@ -1,12 +1,15 @@
 class UsersBackoffice::ProfileController < UsersBackofficeController
   before_action :verify_password, only: %i[ update ]
   before_action :set_user, only: %i[ edit update ]
-  def edit;end
+
+  def edit
+    @user.build_user_profile if @user.user_profile.blank?
+  end
   
   def update
     if @user.update(user_params)
+      bypass_sign_in @user
       redirect_to users_backoffice_profile_path, notice: 'Atualizado com successo'
-      sign_in(@user, bypass: true)
     else
       render :edit, status: :unprocessable_entity
     end
@@ -19,9 +22,9 @@ class UsersBackoffice::ProfileController < UsersBackofficeController
   end
 
   def user_params
-    params.require(:user).permit(:name, :first_name, :last_name, :email, :password, :password_confirmation)
+   params.require(:user).permit(:name, :first_name, :last_name, :email, :password, :password_confirmation, user_profile_attributes: [:id, :address, :gender, :birthdate])
   end
-
+ 
   def verify_password
     if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
       params[:user].extract!(:password, :password_confirmation)
