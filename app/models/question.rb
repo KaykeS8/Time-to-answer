c6bd49800256109f55a73164af9ca5d9b5549f2e
kayke simao <89744379+KaykeS8@users.noreply.github.com>
2024-01-15 10:48:26 -0300

    método set_statistic adicionado e after_create, apos o cadastro de uma questão o método é invocado e soma +1 no value

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,7 @@ class Question < ApplicationRecord
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
   paginates_per 5
+  after_create :set_statistic
   
   scope :search_question, -> (term, page) {
     includes(:answers).where("lower(description) LIKE ?", "%#{term.downcase}%").page(page)
@@ -10,10 +11,16 @@ class Question < ApplicationRecord
 
   scope :search_subject, -> (page, subject_id) {
     includes(:answers, :subject).where(subject_id: subject_id).page(page)
-}
+  }
 
   scope :last_questions, -> (page) {
     includes(:answers).order('created_at desc').page(page)
-}
+  }
+
+  private
+
+  def set_statistic
+    AdminStatistic.set_event(AdminStatistic::EVENTS[:total_questions])
+  end
 
 end
