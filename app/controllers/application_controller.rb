9974291eec13b8f4a7fcc6459d5afb1c21eb2dc9
kayke simao <89744379+KaykeS8@users.noreply.github.com>
2024-01-16 14:26:52 -0300

    Criação de uma variavel global para o model user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   layout :layout_by_resource
+  before_action :set_global_params
 
   protected
 
   def layout_by_resource
     return "#{resource_class.to_s.downcase}_devise" if devise_controller?
     "aplication"
+  end
+
+
+  def set_global_params
+    $global_params = params
   end
 end
